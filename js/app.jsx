403548d0984b406db98a9420d7ce89a73f71060d
Anthonie Lance <anthonielance@users.noreply.github.com>
2017-04-09 12:37:31 -0400

    Add events

--- js/app.jsx
+++ js/app.jsx
@@ -1,6 +1,6 @@
 const Book = (props) => {
     return (
-        <div className="answer">
+        <div className="answer" onClick={() => props.checkAnswer(props.title)}>
             <h4>{props.title}</h4>
         </div>
     );
@@ -12,13 +12,13 @@ Book.propTypes = {
 */
 
 class Quiz extends React.Component {
-    
-    static selectQuiz = (data) => {
-        let books = _.shuffle(data.reduce((p,c,i) => {
-            return p.concat(c.books);
-        }, [])).slice(0,4);
 
-        let answer = books[_.random(books.length-1)];
+    static selectQuiz = (data) => {
+        let books = _.shuffle(data.reduce((p, c, i) => {
+            return p.concat(c.books);
+        }, [])).slice(0, 4);
+
+        let answer = books[_.random(books.length - 1)];
 
         return {
             books: books,
@@ -30,21 +30,52 @@ class Quiz extends React.Component {
         };
     }
 
-    state = Quiz.selectQuiz(this.props.data);
+    static initialState = (data) => {
+        return _.extend({
+            bgClass: 'neutral',
+            showContinue: false
+        }, Quiz.selectQuiz(data))
+    }
+
+    state = Quiz.initialState(this.props.data);
+
+    checkAnswer = (title) => {
+        let isCorrect = this.state.author.books.some((t) => {
+            return t === title;
+        });
+        this.setState((prevState) => ({
+            bgClass: isCorrect ? "pass" : "fail",
+            showContinue: isCorrect
+        }));
+    }
+
+    newQuiz = () => this.setState(Quiz.initialState(this.props.data));
 
     render() {
         return (
-            <div className="row">
-                <div className="col-sm-4">
-                    <img src={this.state.author.imageUrl} className="authorimage" />
+            <div>
+                <div className="row">
+                    <div className="col-sm-4">
+                        <img src={this.state.author.imageUrl} className="authorimage" />
+                    </div>
+                    <div className="col-sm-7">
+                        {this.state.books.map((title, i) =>
+                            <Book key={i} title={title} checkAnswer={this.checkAnswer} />
+                        )}
+                    </div>
+                    <div className={"col-sm-1 " + this.state.bgClass}>
+                    </div>
                 </div>
-                <div className="col-sm-7">
-                    {this.state.books.map((title, i) =>
-                        <Book key={i} title={title} />
-                    )}
-                </div>
-                <div className="col-sm-1">
-                </div>
+                {this.state.showContinue ? (
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <input onClick={this.newQuiz} 
+                                   type="button"
+                                   className="btn btn-primary btn-lg pull-right"
+                                   value="Continue" />
+                        </div>
+                    </div>
+                ) : <span />}
             </div>
         );
     }
@@ -78,19 +109,19 @@ const data = [
         imageUrl: 'images/authors/stephenking.jpg',
         imageSource: 'Wikimedia Commons',
         imageAttribution: 'Pinguino',
-        books: ['The Shining','IT']
+        books: ['The Shining', 'IT']
     },
     {
         name: 'Charles Dickens',
         imageUrl: 'images/authors/charlesdickens.jpg',
         imageSource: 'Wikimedia Commons',
-        books: ['David Copperfield','A Tale of Two Cities']
+        books: ['David Copperfield', 'A Tale of Two Cities']
     },
     {
         name: 'William Shakespeare',
         imageUrl: 'images/authors/williamshakespeare.jpg',
         imageSource: 'Wikimedia Commons',
-        books: ['Hamlet','Macbeth','Romeo and Juliet']
+        books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
     }
 
 ]
